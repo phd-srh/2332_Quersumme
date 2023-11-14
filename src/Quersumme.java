b8166a3e0d6214cd41ebd4bd841c530301c19c41
Dr. Dillinger <peter.dillinger@srh.de>
2023-11-14 09:13:24 +0100

    Initial commit

---
+++ src/Quersumme.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Quersumme {
+    public static void main(String[] args) {
+        Scanner eingabe = new Scanner(System.in);
+        System.out.print("Bitte geben Sie die Zahl ein: ");
+        int zahl = eingabe.nextInt();
+
+        System.out.println("Die Quersumme der Zahl " + zahl + " lautet:");
+
+        // TODO
+    }
+}
